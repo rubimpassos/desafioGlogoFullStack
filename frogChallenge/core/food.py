3e4c1d5d72188aa2a6fdab12501d14aa06785a10
Rubi Mendes Passos <rubimpassos@gmail.com>
2022-04-04 00:38:13 -0300

    add def para retornar dados tratados dos arquivos

---
+++ frogChallenge/core/food.py
@@ -0,0 +1,19 @@
+import re
+
+import pandas as pd
+from numpy import float64
+
+
+def get_foods_from_file(file_path):
+    with open(file_path, encoding="utf-8") as f:
+        lines = f.readlines()
+        data = [
+            [col.strip().replace('\n', '') for col in re.split(r"\s{3,}|\t+", line)]
+            for line in lines[2:]
+        ]
+
+        return pd.DataFrame(
+            data,
+            columns=["NOME", "QUANTIDADE", "PROTEINAS", "CARBOIDRATOS", "GORDURA"],
+            dtype=float64,
+        )
